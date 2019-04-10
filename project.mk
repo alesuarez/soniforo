0097a63a9b65110b952251d0fc5c41619bbf91ef
Sebastian Alejandro Suarez <sas.tuc@gmail.com>
2019-04-10 20:17:32 -0300

    Base freeRTOS - Led parpadeando

---
+++ project.mk
@@ -0,0 +1,19 @@
+#==============================================================================
+# Select a project to compile, or comment and uncomment an example
+#==============================================================================
+
+# ---------- Example of path inside this file folder -------------------
+# Project path
+#(relative to this folder, leave void if the project is in this folder)
+#PROJECT_PATH = examples/c/sapi/bare_metal/gpio
+# Project name
+#PROJECT_NAME = gpio_blinky
+
+# ---------- Example of path outside this file folder ------------------
+# Project path
+#PROJECT_PATH = ../examples
+# Project name
+#PROJECT_NAME = example
+
+PROJECT_PATH = 
+PROJECT_NAME = freeRTOS
