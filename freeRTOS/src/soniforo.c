13fdb1e371578a01f1a426668426ebcec53b46fa
Sebastian Alejandro Suarez <sas.tuc@gmail.com>
2019-04-20 15:24:57 -0300

    Interrupciones configuradas correctamente

--- freeRTOS/src/soniforo.c
+++ freeRTOS/src/soniforo.c
@@ -8,15 +8,20 @@
 DEBUG_PRINT_ENABLE
 ;
 
-SemaphoreHandle_t xSemaphoreTEC1 = NULL;
+SemaphoreHandle_t xSemaphoreTEC1_P = NULL;
+SemaphoreHandle_t xSemaphoreTEC1_L = NULL;
 
 void tec1(void *p) {
-	portTickType xPeriodicity = 500 / portTICK_RATE_MS;
+	portTickType xPeriodicity = 40 / portTICK_RATE_MS;
 	portTickType xLastWakeTime = xTaskGetTickCount();
 	while (1) {
-		if ( xSemaphoreTake( xSemaphoreTEC1, portMAX_DELAY) == pdTRUE) {
-			debugPrintlnString("Tecla 1 task \r\n.");
-			vTaskDelay(2000);
+		if ( xSemaphoreTake( xSemaphoreTEC1_P, portMAX_DELAY) == pdTRUE) {
+			debugPrintlnString("Tecla 1 presionada \r\n");
+
+		}
+
+		if ( xSemaphoreTake( xSemaphoreTEC1_L, portMAX_DELAY) == pdTRUE) {
+			debugPrintlnString("Tecla 1 liberada \r\n");
 
 		}
 		vTaskDelayUntil(&xLastWakeTime, xPeriodicity);
@@ -24,14 +29,15 @@ void tec1(void *p) {
 }
 
 void initIRQ() {
-	Chip_PININT_Init(LPC_GPIO_PIN_INT);
+		Chip_PININT_Init(LPC_GPIO_PIN_INT);
 
-	Chip_SCU_GPIOIntPinSel(0, 0, 4); //Mapeo del pin donde ocurrirá el evento y
+		Chip_SCU_GPIOIntPinSel(0, 3, 4); //Mapeo del pin donde ocurrirá el evento y
 
-	Chip_PININT_SetPinModeEdge(LPC_GPIO_PIN_INT, PININTCH0);//Se configura el canal
-	Chip_PININT_EnableIntLow(LPC_GPIO_PIN_INT, PININTCH0);//Se configura para que el
-
-	NVIC_EnableIRQ(PIN_INT0_IRQn);
+		Chip_PININT_SetPinModeEdge(LPC_GPIO_PIN_INT, PININTCH0);//Se configura el canal
+		Chip_PININT_EnableIntLow(LPC_GPIO_PIN_INT, PININTCH0);//Se configura para que el
+		Chip_PININT_EnableIntHigh(LPC_GPIO_PIN_INT, PININTCH0);//Se configura para que el
+		NVIC_SetPriority( PIN_INT0_IRQn, 5 );
+		NVIC_EnableIRQ(PIN_INT0_IRQn);
 }
 
 void GPIO0_IRQHandler(void){
@@ -39,22 +45,32 @@ void GPIO0_IRQHandler(void){
 
 	if (Chip_PININT_GetFallStates(LPC_GPIO_PIN_INT) & PININTCH0){ //Verificamos que la interrupción es la esperada
 		Chip_PININT_ClearIntStatus(LPC_GPIO_PIN_INT, PININTCH0); //Borramos el flag de interrupción
-		xSemaphoreGiveFromISR( xSemaphoreTEC1, &xSwitchRequired ); //En este caso libero un semáforo
+		xSemaphoreGiveFromISR( xSemaphoreTEC1_P, &xSwitchRequired ); //En este caso libero un semáforo
+	}
+
+	if (Chip_PININT_GetRiseStates(LPC_GPIO_PIN_INT) & PININTCH0){ //Verificamos que la interrupción es la esperada
+			Chip_PININT_ClearIntStatus(LPC_GPIO_PIN_INT, PININTCH0); //Borramos el flag de interrupción
+			xSemaphoreGiveFromISR( xSemaphoreTEC1_L, &xSwitchRequired ); //En este caso libero un semáforo
 	}
 
 	portEND_SWITCHING_ISR(xSwitchRequired);//Terminar con taskYIELD_FROM_ISR (&xSwitchRequired);
-
 }
 
 int main(void) {
 	boardConfig();
 
+	xSemaphoreTEC1_P = xSemaphoreCreateBinary();
+	xSemaphoreTEC1_L = xSemaphoreCreateBinary();
+
+
 	initIRQ();
+
+
 
 	debugPrintConfigUart( UART_USB, 115200 );
 	debugPrintlnString("Inicio de programa... \r\n");
 
-	xSemaphoreTEC1 = xSemaphoreCreateBinary();
+
 
 	gpioWrite(LED3, ON);
 
