fea31766b0d970bbdac5d97470b290321c819d2c
Sebastian Alejandro Suarez <sas.tuc@gmail.com>
2019-04-20 18:19:09 -0300

    Primera version funcionando

--- freeRTOS/src/soniforo.c
+++ freeRTOS/src/soniforo.c
@@ -6,6 +6,10 @@
 // sAPI header
 #include "sapi.h"
 
+#define RED_LED_PORT		GPIO0
+#define YELLOW_LED_PORT		GPIO1
+#define GREEN_LED_PORT 		GPIO2
+
 DEBUG_PRINT_ENABLE
 ;
 
@@ -18,67 +22,144 @@ SemaphoreHandle_t xsYellowLigthOn = NULL;
 SemaphoreHandle_t xsGreenLightOff = NULL;
 SemaphoreHandle_t xsGreenLigthOn = NULL;
 
+typedef enum{
+	LED_ON,
+	LED_OFF
+} Led_Status;
+
+typedef enum{
+	RED_LED,
+	YELLOW_LED,
+	GREEN_LED
+} Led_Name;
+
+struct Message { //Estructura con formato del mensaje
+	Led_Name Led;
+	Led_Status Status;
+	int32_t Time;
+};
+
 void ligthRedTask(void *p) {
-	debounceData_t redInput;
-	initDebounceFsm(&redInput, GPIO0);
-	portTickType xPeriodicity = 40 / portTICK_RATE_MS;
+	xQueueHandle Buffer = *(xQueueHandle *) p;
+	struct Message ligthRedTask_Message;
+
+	portTickType xPeriodicity = 20 / portTICK_RATE_MS;
 	portTickType xLastWakeTime = xTaskGetTickCount();
+
 	while (1) {
+
 		if ( xSemaphoreTake( xsRedLigthOn, portMAX_DELAY) == pdTRUE) {
-			if (gpioRead(GPIO0)) {
-				debugPrintlnString("R\n");
+			if (gpioRead(RED_LED_PORT)) {
+				ligthRedTask_Message.Led = RED_LED;
+				ligthRedTask_Message.Status = LED_ON;
+				xQueueSend(Buffer, &ligthRedTask_Message, portMAX_DELAY);
 			}
 		}
 
 		if ( xSemaphoreTake( xsRedLightOff, portMAX_DELAY) == pdTRUE) {
-			if (!gpioRead(GPIO0)) {
-				debugPrintlnString("r\n");
+			if (!gpioRead(RED_LED_PORT)) {
+				ligthRedTask_Message.Led = RED_LED;
+				ligthRedTask_Message.Status = LED_OFF;
+				// xQueueSend(Buffer, &ligthRedTask_Message, portMAX_DELAY);
 			}
 		}
 
-		vTaskDelayUntil(&xLastWakeTime, xPeriodicity);
+		//vTaskDelayUntil(&xLastWakeTime, xPeriodicity);
 	}
 }
 
 void ligthYellowTask(void *p) {
-	portTickType xPeriodicity = 40 / portTICK_RATE_MS;
+	xQueueHandle Buffer = *(xQueueHandle *) p;
+	struct Message ligthYellowTask_Message;
+
+	portTickType xPeriodicity = 20 / portTICK_RATE_MS;
 	portTickType xLastWakeTime = xTaskGetTickCount();
 	while (1) {
 		if ( xSemaphoreTake( xsYellowLigthOn, portMAX_DELAY) == pdTRUE) {
-			if (gpioRead(GPIO1)) {
-				debugPrintlnString("A\n");
+			if (gpioRead(YELLOW_LED_PORT)) {
+				ligthYellowTask_Message.Led = YELLOW_LED;
+				ligthYellowTask_Message.Status = LED_ON;
+				xQueueSend(Buffer, &ligthYellowTask_Message, portMAX_DELAY);
 			}
 		}
 
 		if ( xSemaphoreTake( xsYellowLightOff, portMAX_DELAY) == pdTRUE) {
-			if (!gpioRead(GPIO1)) {
-				debugPrintlnString("a\n");
+			if (!gpioRead(YELLOW_LED_PORT)) {
+				ligthYellowTask_Message.Led = YELLOW_LED;
+				ligthYellowTask_Message.Status = LED_OFF;
+				// xQueueSend(Buffer, &ligthYellowTask_Message, portMAX_DELAY);
 			}
 		}
 
-		vTaskDelayUntil(&xLastWakeTime, xPeriodicity);
+		//vTaskDelayUntil(&xLastWakeTime, xPeriodicity);
 	}
 }
 
 void ligthGreenTask(void *p) {
-	portTickType xPeriodicity = 40 / portTICK_RATE_MS;
+	xQueueHandle Buffer = *(xQueueHandle *) p;
+	struct Message ligthGreenTask_Message;
+
+	portTickType xPeriodicity = 20 / portTICK_RATE_MS;
 	portTickType xLastWakeTime = xTaskGetTickCount();
 	while (1) {
 		if ( xSemaphoreTake( xsGreenLigthOn, portMAX_DELAY) == pdTRUE) {
-			if (gpioRead(GPIO2)) {
-				debugPrintlnString("V\n");
+			if (gpioRead(GREEN_LED_PORT)) {
+				ligthGreenTask_Message.Led = GREEN_LED;
+				ligthGreenTask_Message.Status = LED_ON;
+				xQueueSend(Buffer, &ligthGreenTask_Message, portMAX_DELAY);
 			}
 		}
 
 		if ( xSemaphoreTake( xsGreenLightOff, portMAX_DELAY) == pdTRUE) {
-			if (!gpioRead(GPIO2)) {
-				debugPrintlnString("v\n");
+			if (!gpioRead(GREEN_LED_PORT)) {
+				ligthGreenTask_Message.Led = GREEN_LED;
+				ligthGreenTask_Message.Status = LED_OFF;
+				// xQueueSend(Buffer, &ligthGreenTask_Message, portMAX_DELAY);
 			}
 		}
 
-		vTaskDelayUntil(&xLastWakeTime, xPeriodicity);
+		//vTaskDelayUntil(&xLastWakeTime, xPeriodicity);
 	}
 }
+
+void copyMessage(struct Message src, struct Message * dst) {
+	dst->Led = src.Led;
+	dst->Status = src.Status;
+	dst->Time = src.Time;
+}
+
+void decideAction(struct Message new, struct Message old) {
+	// todo: In this implementation the turn off leds states were not taken into account
+	if (old.Led == YELLOW_LED && new.Led == GREEN_LED){
+		debugPrintlnString("Cruzar\r\n");
+		return;
+	}
+
+	if (old.Led == GREEN_LED && new.Led == YELLOW_LED) {
+		debugPrintlnString("Apurate\r\n");
+		return;
+	}
+
+	debugPrintlnString("Esperar\r\n");
+	return;
+
+}
+
+void Send(void * a) {
+	xQueueHandle Buffer = *(xQueueHandle *) a;
+	struct Message Sending_Message;
+	static struct Message oldMessage;
+	while (1) {
+
+		if (xQueueReceive(Buffer, &Sending_Message, portMAX_DELAY)){
+			decideAction(Sending_Message, oldMessage);
+			copyMessage(Sending_Message, &oldMessage);
+			vTaskDelay(1 / portTICK_RATE_MS);
+		}
+	}
+}
+
+
 
 void initIRQ() {
 	Chip_PININT_Init(LPC_GPIO_PIN_INT);
@@ -157,6 +238,9 @@ void GPIO2_IRQHandler(void) {
 }
 
 int main(void) {
+
+	static xQueueHandle Send_Buf;
+
 	boardConfig();
 
 	xsRedLightOff = xSemaphoreCreateBinary();
@@ -173,32 +257,42 @@ int main(void) {
 	debugPrintConfigUart( UART_USB, 115200 );
 	debugPrintlnString("Inicio de programa... \r\n");
 
+	Send_Buf = xQueueCreate(10,sizeof(struct Message));
+
 	gpioWrite(LED3, ON);
 
 	// Crear tarea en freeRTOS
 	xTaskCreate(ligthRedTask,                  // Funcion de la tarea a ejecutar
 			(const char *) "ligthRedTask", // Nombre de la tarea como String amigable para el usuario
-			configMINIMAL_STACK_SIZE * 4, // Cantidad de stack de la tarea
-			0,                          // Parametros de tarea
+			configMINIMAL_STACK_SIZE * 2, // Cantidad de stack de la tarea
+			&Send_Buf,                          // Parametros de tarea
 			tskIDLE_PRIORITY + 7,         // Prioridad de la tarea
 			0                         // Puntero a la tarea creada en el sistema
 			);
 
 	xTaskCreate(ligthYellowTask,               // Funcion de la tarea a ejecutar
 			(const char *) "ligthYellowTask", // Nombre de la tarea como String amigable para el usuario
-			configMINIMAL_STACK_SIZE * 4, // Cantidad de stack de la tarea
-			0,                          // Parametros de tarea
+			configMINIMAL_STACK_SIZE * 2, // Cantidad de stack de la tarea
+			&Send_Buf,                          // Parametros de tarea
 			tskIDLE_PRIORITY + 7,         // Prioridad de la tarea
 			0                         // Puntero a la tarea creada en el sistema
 			);
 
 	xTaskCreate(ligthGreenTask,               // Funcion de la tarea a ejecutar
-				(const char *) "ligthGreenTask", // Nombre de la tarea como String amigable para el usuario
-				configMINIMAL_STACK_SIZE * 4, // Cantidad de stack de la tarea
-				0,                          // Parametros de tarea
-				tskIDLE_PRIORITY + 7,         // Prioridad de la tarea
-				0                         // Puntero a la tarea creada en el sistema
-				);
+			(const char *) "ligthGreenTask", // Nombre de la tarea como String amigable para el usuario
+			configMINIMAL_STACK_SIZE * 2, // Cantidad de stack de la tarea
+			&Send_Buf,                          // Parametros de tarea
+			tskIDLE_PRIORITY + 7,         // Prioridad de la tarea
+			0                         // Puntero a la tarea creada en el sistema
+			);
+
+	xTaskCreate(Send,               // Funcion de la tarea a ejecutar
+			(const char *) "Send", // Nombre de la tarea como String amigable para el usuario
+			configMINIMAL_STACK_SIZE * 2, // Cantidad de stack de la tarea
+			&Send_Buf,                          // Parametros de tarea
+			tskIDLE_PRIORITY + 1,         // Prioridad de la tarea
+			0                         // Puntero a la tarea creada en el sistema
+			);
 
 	// Iniciar scheduler
 	vTaskStartScheduler();
