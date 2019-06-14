c2419473b1dbd2d7ddc47e3e186140755c636b99
Sebastian Alejandro Suarez <sas.tuc@gmail.com>
2019-06-13 22:24:47 -0300

    Modificacion del project.yml

--- freeRTOS/test/test_cfg_warng_devices.c
+++ freeRTOS/test/test_cfg_warng_devices.c
@@ -1,9 +1,6 @@
 #include "unity.h"
 // ceedling test:all
-#include "../../libs/sapi/sapi_v0.5.2/base/inc/sapi.h"
-#include "../inc/FreeRTOSConfig.h"
-#include "../../libs/freertos/include/FreeRTOS.h"
-#include "../../libs/vendor_libs/lpc_chip_43xx/inc/chip.h"
-#include "../../libs/vendor_libs/lpc_chip_43xx/inc/cmsis.h"
-#include "../inc/cfg_warng_devices.h"
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "cfg_warng_devices.h"
 //
