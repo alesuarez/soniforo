5c531ceed14d51b972a555238aada9a0c930bb52
Sebastian Alejandro Suarez <sas.tuc@gmail.com>
2019-06-13 22:08:05 -0300

    Agregando test

---
+++ freeRTOS/build/test/preprocess/includes/test_cfg_warng_devices.c
@@ -0,0 +1,6 @@
+---
+- unity.h
+- "../inc/FreeRTOSConfig.h"
+- "../../libs/freertos/include/FreeRTOS.h"
+- "../inc/cfg_warng_devices.h"
+- build/temp/_test_cfg_warng_devices.c
--- freeRTOS/test/test_cfg_warng_devices.c
+++ freeRTOS/test/test_cfg_warng_devices.c
@@ -1,8 +1,9 @@
 #include "unity.h"
 // ceedling test:all
-#include "../inc/soniforo.h"
+#include "../../libs/sapi/sapi_v0.5.2/base/inc/sapi.h"
 #include "../inc/FreeRTOSConfig.h"
 #include "../../libs/freertos/include/FreeRTOS.h"
 #include "../../libs/vendor_libs/lpc_chip_43xx/inc/chip.h"
-
-
+#include "../../libs/vendor_libs/lpc_chip_43xx/inc/cmsis.h"
+#include "../inc/cfg_warng_devices.h"
+//
